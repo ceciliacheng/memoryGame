e4538d97a1b5664b8e949b750418d1b1f1d4dcb2
Cecilia Cheng <1772979678@qq.com>
2018-04-07 10:16:40 +0800

    补回firstcard和secondcard的定义

--- js/app.js
+++ js/app.js
@@ -1,93 +1,101 @@
-const cards = ['<li class="card"><i class="fa fa-diamond"></i></li>',
-          '<li class="card"><i class="fa fa-diamond"></i></li>',
-          '<li class="card"><i class="fa fa-paper-plane-o"></i></li>',
-          '<li class="card"><i class="fa fa-paper-plane-o"></i></li>',
-          '<li class="card"><i class="fa fa-anchor"></i></li>',
-          '<li class="card"><i class="fa fa-anchor"></i></li>',
-          '<li class="card"><i class="fa fa-bolt"></i></li>',
-          '<li class="card"><i class="fa fa-bolt"></i></li>',
-          '<li class="card"><i class="fa fa-cube"></i></li>',
-          '<li class="card"><i class="fa fa-cube"></i></li>',
-          '<li class="card"><i class="fa fa-leaf"></i></li>',
-          '<li class="card"><i class="fa fa-leaf"></i></li>',
-          '<li class="card"><i class="fa fa-bicycle"></i></li>',
-          '<li class="card"><i class="fa fa-bicycle"></i></li>',
-          '<li class="card"><i class="fa fa-bomb"></i></li>',
-          '<li class="card"><i class="fa fa-bomb"></i></li>'];
+const cards = [
+  '<li class="card"><i class="fa fa-diamond"></i></li>',
+  '<li class="card"><i class="fa fa-diamond"></i></li>',
+  '<li class="card"><i class="fa fa-paper-plane-o"></i></li>',
+  '<li class="card"><i class="fa fa-paper-plane-o"></i></li>',
+  '<li class="card"><i class="fa fa-anchor"></i></li>',
+  '<li class="card"><i class="fa fa-anchor"></i></li>',
+  '<li class="card"><i class="fa fa-bolt"></i></li>',
+  '<li class="card"><i class="fa fa-bolt"></i></li>',
+  '<li class="card"><i class="fa fa-cube"></i></li>',
+  '<li class="card"><i class="fa fa-cube"></i></li>',
+  '<li class="card"><i class="fa fa-leaf"></i></li>',
+  '<li class="card"><i class="fa fa-leaf"></i></li>',
+  '<li class="card"><i class="fa fa-bicycle"></i></li>',
+  '<li class="card"><i class="fa fa-bicycle"></i></li>',
+  '<li class="card"><i class="fa fa-bomb"></i></li>',
+  '<li class="card"><i class="fa fa-bomb"></i></li>'
+];
 
-let opencards = [];
+let opencards = [],
+  firstCard = "",
+  secondCard = "";
 
 //重启整个画面
-function initGame(){
+function initGame() {
   $(".deck").empty();
   match = 0;
   moves = 0;
-  $('.move').text('0');
-  $('.fa-star').removeClass('fa-star-o').addClass('fa-star');
+  $(".move").text("0");
+  $(".fa-star")
+    .removeClass("fa-star-o")
+    .addClass("fa-star");
   resetTimer();
-  $('.timer').text('0');
+  $(".timer").text("0");
   creatCards();
   $(".card").click(show);
 }
 
-function creatCards(){
- cardList = shuffle(cards);
- cardList.forEach(addCard);
+function creatCards() {
+  cardList = shuffle(cards);
+  cardList.forEach(addCard);
 }
 
-function addCard(card){
+function addCard(card) {
   $(".deck").append($(card));
 }
 
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
-function enableClick(){
+function enableClick() {
   opencards[0].click(show);
 }
 
 function show() {
   if (opencards.length === 0) {
-    $(this).toggleClass('show open');
+    $(this).toggleClass("show open");
     opencards.push($(this));
     firstCard = this.firstChild.className;
     disableClick();
   } else if (opencards.length === 1) {
     //updateMoves();
     updateMoves();
-    $(this).toggleClass('show open');
+    $(this).toggleClass("show open");
     opencards = this.firstChild.className;
-    //setTimeout(matchCards,500);
+    setTimeout(matchCards, 500);
   }
 
-  console.log(this)
-  console.log(opencards)
+  console.log(this);
+  console.log(opencards);
 }
 
-function disableClick(){
-  opencards.forEach(function(card){
-    card.off('click');
+function disableClick() {
+  opencards.forEach(function(card) {
+    card.off("click");
   });
 }
 
-function matchCards(){
+function matchCards() {
   if (firstCard === secondCard) {
     opencards[0].addClass("match");
     opencards[1].addClass("match");
     //removeOpenCards();
     //setTimeout(checkResult,500);
-  }else{
+  } else {
     opencards[0].toggleClass("show open");
     opencards[1].toggleClass("show open");
     enableClick();
@@ -95,17 +103,15 @@ function matchCards(){
   }
 }
 
-function checkResult(){
-
-}
+function checkResult() {}
 //function check() {
-  //$(this).toggleClass("show card");
-  //opencards.push($(this));
-  //console.log(opencards)
+//$(this).toggleClass("show card");
+//opencards.push($(this));
+//console.log(opencards)
 //}
 
 //计时器
-function timer(){
+function timer() {
   let startTime = new Date().getTime();
   timer = setInterval(function() {
     let currenTime = new Date().getTime();
@@ -114,39 +120,36 @@ function timer(){
     let secs = Math.floor((timePlayed % (1000 * 60)) / 1000);
     timerValue = mins + " minutes " + secs + "seconds";
     if (secs < 10) {
-                secs = "0" + secs;
-            }
-            if (mins < 10) {
-                mins = "0" + mins;
-            }
+      secs = "0" + secs;
+    }
+    if (mins < 10) {
+      mins = "0" + mins;
+    }
 
-            let lastCurrentTime = mins + ':' + secs;
+    let lastCurrentTime = mins + ":" + secs;
 
-            // Update timer on game screen and modal
-            $(".timer").text(lastCurrentTime);
-  },500)
+    // Update timer on game screen and modal
+    $(".timer").text(lastCurrentTime);
+  }, 500);
 }
 
-function updateMoves(){
+function updateMoves() {}
 
+$(".deck").on("click", "li", function() {
+  //显示卡片
+  //show.call(this);
+  //opencards.push(this);
+});
+
+$(".restart").on("click", function() {
+  initGame();
+});
+
+//重启时间
+function resetTimer() {
+  if (timer) {
+    clearInterval(timer);
+  }
 }
 
- $(".deck").on("click","li",function(){
-   //显示卡片
-   //show.call(this);
-   //opencards.push(this);
- });
-
- $(".restart").on("click",function(){
-   initGame();
-
- });
-
- //重启时间
- function resetTimer(){
-   if(timer) {
-     clearInterval(timer);
-     }
- }
-
- initGame();
+initGame();
